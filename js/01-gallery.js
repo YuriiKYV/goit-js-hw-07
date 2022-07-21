34560fce63cdfb200c2e13852fb070ae44bf9362
Yurii Kukharuk <yurakukharuk@gmail.com>
2022-07-21 11:32:24 +0300

    Сompleted tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+function createItem(array) {
+    return array.reduce((acc, item) => acc + `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+    </a>
+    </div>`, ''); 
+}
+
+const list = createItem(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+gallery.insertAdjacentHTML('beforeend', list);
+
+function imgHandler(e) {
+  e.preventDefault();
+  const instance = basicLightbox.create(`
+    <img 
+    src="${e.target.dataset.source}" 
+    width="800" height="600"
+    alt="${e.target.alt}">
+`);
+  
+  instance.show();
+}
+gallery.addEventListener('click', imgHandler);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+function createItem(array) {
+    return array.reduce((acc, item) => acc + `
+    <a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" 
+    src="${item.preview}" 
+    alt="${item.description}" />
+    </a>`, ''); 
+}
+
+const list = createItem(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+gallery.insertAdjacentHTML('beforeend', list);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
